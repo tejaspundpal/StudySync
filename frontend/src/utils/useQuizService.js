ca056152b35e58ac9865cc87fa336b407a273646
tejaspundpal <96496782+tejaspundpal@users.noreply.github.com>
2024-07-17 19:05:33 +0530

    update

--- frontend/src/utils/useQuizService.js
+++ frontend/src/utils/useQuizService.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 export const api = axios.create({
-	baseURL: "http://localhost:8182/api/quiz"
+  baseURL: "http://localhost:8182/api/quiz"
 })
 
-export const createQuestion = async(quizQustion) =>{
+export const createQuestion = async (quizQustion) => {
   try {
     const response = await api.post("/create-new-question", quizQustion)
     return response.data
@@ -13,7 +13,7 @@ export const createQuestion = async(quizQustion) =>{
   }
 }
 
-export const getAllQuestions = async() =>{
+export const getAllQuestions = async () => {
   try {
     const response = await api.get("/all-questions")
     return response.data
@@ -23,12 +23,49 @@ export const getAllQuestions = async() =>{
   }
 }
 
-export const getSubjects = async() =>{
-    try {
-      const response = await api.get("/subjects")
-      return response.data
-    } catch (error) {
-      console.error(error)
-      
-    }
+export const getSubjects = async () => {
+  try {
+    const response = await api.get("/subjects")
+    return response.data
+  } catch (error) {
+    console.error(error)
+
   }
+}
+
+export const fetchQuizForUser = async (number,subject) => {
+  try {
+    const response = await api.get(`/fetch-questions-for-user?noOfQuestions=${number}&subject=${subject}`)
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return []
+  }
+}
+
+export const updateQuestion = async (id,question) => {
+  try {
+    const response = await api.put(`/question/${id}/update`,question);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const getQuestionById = async (id) => {
+  try {
+    const response = await api.get(`/question/${id}`)
+    return response.data
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const deleteQuestion = async(id) => {
+  try {
+    const response = await api.delete(`/question/${id}/delete`)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
